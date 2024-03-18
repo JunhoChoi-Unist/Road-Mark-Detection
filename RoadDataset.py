1f86e4df6d6af8296ebedc29c6af95166eed547b
Junho Choi <junhochoi@unist.ac.kr>
2024-03-19 00:57:10 +0900

    Dataset dtype changed

--- RoadDataset.py
+++ RoadDataset.py
@@ -12,9 +12,6 @@ class RoadDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        '''
-        TODO: ds should return (rbg-3x480x640, seg-120x160, vp-x,y)
-        '''
         import scipy.io
         data = scipy.io.loadmat(self.data[idx])
         data = data['rgb_seg_vp']
@@ -23,18 +20,21 @@ class RoadDataset(Dataset):
         seg = torch.Tensor(data[:, :, 3])
         vp = data[:, :, 4]
 
-        if vp.max() == 1:
-            vp_x = vp.sum(axis=0).argmax(axis=0)
-            vp_y = vp.sum(axis=1).argmax(axis=0)
-            vpxy = torch.Tensor((vp_x, vp_y,))
-        else:
-            vpxy = torch.Tensor(0, 0,)
-
         if self.transform is not None:
             rgb = self.transform(rgb)
 
         if self.small_seg:
             seg = F.max_pool2d(seg.unsqueeze(0), 4).squeeze(0)
+            # TODO: change interpolation method from max value to argmax?
+        seg = seg.long()
+
+        if vp.max() == 1:
+            vp_x = vp.sum(axis=0).argmax(axis=0)
+            vp_y = vp.sum(axis=1).argmax(axis=0)
+            vpxy = torch.Tensor((vp_x, vp_y,))
+        else:
+            vpxy = torch.Tensor((0, 0,))
+        vpxy = vpxy.long()
 
         return rgb, seg, vpxy
 
@@ -44,6 +44,6 @@ if __name__=='__main__':
     _, _val, _ = train_test_split(root_dir='D:/VPGNet-DB-5ch/', val_size=0.15)    
     _dsds = RoadDataset(_val, T.Compose([T.ToTensor()]))
     _rgb, _seg, _vp = _dsds[0]
-    print(f"_rgb.shape:{_rgb.shape}")
-    print(f"_seg.shape:{_seg.shape}")
-    print(f"_vp:{_vp}")
+    print(f"_rgb: {_rgb.shape} {_rgb.dtype}")
+    print(f"_seg: {_seg.shape} {_seg.dtype}")
+    print(f"_vp: {_vp} {_vp.dtype}")
