1d4578d07620e0211d3ba03a8f6c9041de241286
Junho Choi <junhochoi@unist.ac.kr>
2024-03-20 13:51:02 +0900

    delete backup

--- losses/__init__.py
+++ losses/__init__.py
@@ -12,13 +12,10 @@ class FourTaskLoss(nn.Module):
             self.weights = torch.tensor(weights)
 
     def forward(self, out, gridbox, seg, vpxy):
-        # print(f"gridbox: {gridbox.shape} {gridbox.dtype} {gridbox.device}")
         l_reg = F.l1_loss(out[0], gridbox.to('cuda'))
         om = (seg > 0).long()
-        # print(f"om: {om.shape} {om.dtype} {om.device}")
         l_om = F.cross_entropy(out[1], om.to('cuda'))
         ml_seg = torch.nn.functional.max_pool2d(seg, 2).long()
-        # print(f"ml_seg: {ml_seg.shape} {ml_seg.dtype} {ml_seg.device}")
         l_ml = F.cross_entropy(out[2], ml_seg.to('cuda'))
         vp = torch.zeros((out[3].shape[0], 120, 160)).long()
         for i, (vp_x, vp_y) in enumerate(vpxy):
@@ -27,7 +24,6 @@ class FourTaskLoss(nn.Module):
                 vp[i][: vp_y // 4, : vp_x // 4] = 2
                 vp[i][vp_y // 4 :, : vp_x // 4] = 3
                 vp[i][vp_y // 4 :, vp_x // 4 :] = 4
-        # print(f"vp: {vp.shape} {vp.dtype} {vp.device}")
         l_vp = F.cross_entropy(out[3], vp.to('cuda'))
         # total_loss = torch.zeros(1, requires_grad=True, device='cuda')
         # for weight, loss in zip(self.weights, [l_reg, l_om, l_ml, l_vp]):
